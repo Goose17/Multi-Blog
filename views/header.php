24fa7cd490a74dab1483f8355884cafe6acbb472
JamieKuppel <jamiekuppel@gmail.com>
2016-04-08 12:36:53 -0400

    Header/footer
    Worked on a draft for the top navbar

---
+++ views/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Blog</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    </head>
+    <body>
+        <nav class="navbar-default navbar-fixed-top navbar">
+            <div class="container">
+                <a href="#" class="navbar-brand">Home</a>
+                <button class="navbar-btn btn btn-primary navbar-right">Create post</button>
+                <button class="navbar-btn btn btn-primary navbar-right" style="padding-left: 20px;">Sign In/Register</button>
+            </div>
+        </nav>
+    
