399a47f96e118daa806d91ec14b16d1f98a8f3e1
Goose17 <bbrobe12@stlawu.edu>
2016-04-11 12:29:17 -0400

    Consolidation and addition to post controller
    All database addition and deletion is now being handled by one
    controller. Functions for adding posts and comments are added. Posts in
    the database don't need titles because comments don't have titles.

--- models/posts.php
+++ models/posts.php
@@ -5,14 +5,32 @@ function request($db) {
     return $table;
 }
 
-function Drop($id, $db){
+function drop($id, $db){
         
     $drop = $db->prepare("delete from Posts where id=:id;");
     $drop->bindParam(':id', $id, PDO::PARAM_INT);
     $drop->execute();
 }
     
-function Add($title, $content, $userName, $db){
+function addPost($title, $content, $userName, $db){
+            
+    $insert = $db->prepare('insert into Posts(title, content, username) values(:title, :content, :username);');
+    $insert->bindParam(':title', $title, PDO::PARAM_STR);
+    $insert->bindParam(':content', $content, PDO::PARAM_STR);
+    $insert->bindParam(':username', $userName, PDO::PARAM_STR);
+    $insert->execute();
+}
+
+function addComment($parent, $content, $userName, $db){
+            
+    $insert = $db->prepare('insert into Posts(title, content, username) values(:title, :content, :username);');
+    $insert->bindParam(':title', $title, PDO::PARAM_STR);
+    $insert->bindParam(':content', $content, PDO::PARAM_STR);
+    $insert->bindParam(':username', $userName, PDO::PARAM_STR);
+    $insert->execute();
+}
+
+function ratingUp($postid, $db){
             
     $insert = $db->prepare('insert into Posts(title, content, username) values(:title, :content, :username);');
     $insert->bindParam(':title', $title, PDO::PARAM_STR);
---
+++ postController.php
@@ -0,0 +1,37 @@
+<?php // Controller for Posts generation
+
+session_start();
+
+if (isset($_POST['task'])){
+
+    // Connect to database
+    require_once('models/database.php');
+    $db = databaseConnection();
+    
+    if (!isset($db)) {
+        $error = "Could not connect to the database.";
+    } else {
+        
+        if ($_POST['task'] == 'addPost' && isset($_POST['title']) && isset($_POST['content'])) {
+           
+           require_once('models/posts.php');
+           addPost($_POST['title'], $_POST['content'], $_SESSION['userName'], $db);
+           
+        }
+        
+        if ($_POST['task'] == 'addComment' && isset($_POST['parent']) && isset($_POST['content'])) {
+           
+           require_once('models/posts.php');
+           addComment($_POST['parent'], $_POST['content'], $_SESSION['userName'], $db);
+           
+        }
+        
+        if ($_POST['task'] == 'dropPost' && isset($_POST['postid'])) {
+           
+           require_once('models/posts.php');
+           drop($_POST['postid'], $db);
+           
+        }
+        
+    }
+}
