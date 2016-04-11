ddb25c700c8fb31c92685dda28965a5abdf92587
Goose17 <bbrobe12@stlawu.edu>
2016-04-11 12:44:54 -0400

    Work on functions
    Continued work on posts.php functions and added the function to increase
    a post's rating. This function, along with all other functions, IS YET
    UNTESTED.

--- models/posts.php
+++ models/posts.php
@@ -4,13 +4,6 @@ function request($db) {
     $table = $db->query('select * from Posts order by time_stamp;'); 
     return $table;
 }
-
-function drop($id, $db){
-        
-    $drop = $db->prepare("delete from Posts where id=:id;");
-    $drop->bindParam(':id', $id, PDO::PARAM_INT);
-    $drop->execute();
-}
     
 function addPost($title, $content, $userName, $db){
             
@@ -23,18 +16,23 @@ function addPost($title, $content, $userName, $db){
 
 function addComment($parent, $content, $userName, $db){
             
-    $insert = $db->prepare('insert into Posts(title, content, username) values(:title, :content, :username);');
-    $insert->bindParam(':title', $title, PDO::PARAM_STR);
+    $insert = $db->prepare('insert into Posts(parent, content, username) values(:parent, :content, :username);');
+    $insert->bindParam(':parent', $title, PDO::PARAM_INT);
     $insert->bindParam(':content', $content, PDO::PARAM_STR);
     $insert->bindParam(':username', $userName, PDO::PARAM_STR);
     $insert->execute();
 }
 
+function drop($postid, $db){
+        
+    $drop = $db->prepare("delete from Posts where post_id=:id;");
+    $drop->bindParam(':id', $postid, PDO::PARAM_INT);
+    $drop->execute();
+}
+
 function ratingUp($postid, $db){
             
-    $insert = $db->prepare('insert into Posts(title, content, username) values(:title, :content, :username);');
-    $insert->bindParam(':title', $title, PDO::PARAM_STR);
-    $insert->bindParam(':content', $content, PDO::PARAM_STR);
-    $insert->bindParam(':username', $userName, PDO::PARAM_STR);
+    $insert = $db->prepare('update Posts set rating = rating + 1 where post_id=:id;');
+    $insert->bindParam(':id', $postid, PDO::PARAM_INT);
     $insert->execute();
 }
