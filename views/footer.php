f8f972ade003bef6c660c845184b1f4ec939168b
JamieKuppel <jamiekuppel@gmail.com>
2016-04-17 17:50:13 -0400

    Register Form jQuery validation
    Validation of the register form works and has some nice visual effects
    added on.

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,52 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="views/increment.js"></script>
-    <script src="views/login-validate.js"></script>
+    <script>
+        $(document).ready(function() {
+            
+            // Helper function returns true when form is ok to be submitted and false otherwise.
+            function validate() {
+                // Check to make sure username input is set and is at least 1 character.
+                var username = $('#username-signup').val().length > 0;
+                
+                // Check to make sure password input is set and is at least 6 characters.
+                var password = $('#password-signup').val().length > 5;
+                
+                // Confirms that both password input and confirm password input are the same value.
+                var confirm = $('#password-signup').val() == $('#password-confirm-signup').val();
+                
+                return username && password && confirm;
+            }
+            
+            // Makes sure we can't submit a form before all fields are good to go.
+            $(document).on('mouseover', function() {
+                var button = $('#signup-submit');
+                validate() ? button.removeAttr('disabled') : button.attr('disabled', 'true');
+            });
+            
+            // Flashy effects for password being under 6 characters.
+            $('#password-signup').on('keyup', function() {
+                if ($(this).val().length < 6) {
+                    $(this).parent().addClass('has-error');
+                    $(this).siblings('p').removeClass('hidden');
+                } else {
+                    $(this).parent().removeClass('has-error');
+                    $(this).siblings('p').addClass('hidden');
+                }
+            });
+            
+            // Effects for mismatching passwords.
+            $('#password-confirm-signup').on('keyup', function() {
+               if ($(this).val() != $('#password-signup').val()) {
+                    $(this).parent().addClass('has-error');
+                    $(this).siblings('p').removeClass('hidden');
+               } else {
+                    $(this).parent().removeClass('has-error');
+                    $(this).siblings('p').addClass('hidden');
+               }
+            });
+            
+        });
+    </script>
   </body>
 </html>
--- views/sign-in-register-forms.php
+++ views/sign-in-register-forms.php
@@ -1,21 +1,23 @@
         <div class="container">
             <div class="row">
-                <form method="post" action="sign-in-control.php" class="col-sm-12 col-md-6">
+                <form method="post" action="sign-in-control.php" class="col-sm-12 col-md-6" id="register-form">
                     <h4>Register</h4>
                     <?php if (isset($failed_register_message)) { echo "<div class='alert alert-danger' role='alert'><p>$failed_register_message</p></div>";} ?>
                     <div class="form-group">
-                        <label for="username-signup">Username</label>
+                        <label for="username-signup control-label">Username</label>
                         <input type="text" class="form-control" id="username-signup" placeholder="Username" name="username_register">
                     </div>
                     <div class="form-group">
-                        <label for="password-signup">Password</label>
+                        <label for="password-signup" class="control-label">Password</label>
                         <input type="password" class="form-control" id="password-signup" placeholder="Password" name="password_register">
+                        <p class="hidden control-label">Password must be at least 6 characters.</p>
                     </div>
                     <div class="form-group">
-                        <label for="password-confirm-signup">Confirm Password</label>
+                        <label for="password-confirm-signup" class="control-label">Confirm Password</label>
                         <input type="password" class="form-control" id="password-confirm-signup" placeholder="Confirm Password">
+                        <p class="hidden control-label">Passwords do not match.</p>
                     </div>
-                    <input type="submit" class="btn btn-primary" value="Submit">
+                    <input type="submit" class="btn btn-primary" value="Submit" id="signup-submit" disabled="true">
                 </form>
                 <form method="post" action="sign-in-control.php" class="col-sm-12 col-md-6">
                     <h4>Login</h4>
