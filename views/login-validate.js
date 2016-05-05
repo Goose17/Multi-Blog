18cf6693e23cb78814c1925e31d5394207780e60
JamieKuppel <jamiekuppel@gmail.com>
2016-05-04 21:07:40 -0400

    Updated form validation
    went back and fixed register form validation to work more efficiently

--- views/login-validate.js
+++ views/login-validate.js
@@ -1,69 +1,56 @@
         $(document).ready(function() {
-            
+
             $("form input").eq(0).focus();
-            
+
             // Helper function returns true when form is ok to be submitted and false otherwise.
             function validate() {
                 // Check to make sure username input is set and is at least 1 character.
                 var username = $('#username-signup').val().length > 0;
-                
+
                 // Check to make sure password input is set and is at least 6 characters.
                 var password = $('#password-signup').val().length > 5;
-                
+
                 // Confirms that both password input and confirm password input are the same value.
                 var confirm = $('#password-signup').val() == $('#password-confirm-signup').val();
-                
+
                 return username && password && confirm;
             }
-            
-            function confirm() {
-                var password = $('#password-signup');
-                var confirm = $('#password-confirm-signup');
-                if (password.val()) {
-                    if (confirm.val() != password.val()) {
-                        confirm.parent().addClass('has-error');
-                        confirm.siblings('p').removeClass('hidden');
-                    } else {
-                        confirm.parent().removeClass('has-error');
-                        confirm.siblings('p').addClass('hidden');
-                    }
-                }
-            }
-            
-            // Makes sure we can't submit a form before all fields are good to go.
-            $(document).on('mouseover', function() {
-                var button = $('#signup-submit');
-                validate() ? button.removeAttr('disabled') : button.attr('disabled', 'true');
+
+            $('#signup-submit').click(function(e) {
+              if (!$('#username-signup').val()) {
+                e.preventDefault();
+                $('#username-warning').removeClass('hidden');
+                $('#username-warning').parent().parent().addClass('has-error');
+              } else if (!$('#password-signup').val() || $('#password-signup').val().length < 6) {
+                e.preventDefault();
+                $('#p_warning').removeClass('hidden');
+                $('#p_warning').parent().parent().addClass('has-error');
+              } else if (!$('#password-confirm-signup').val() || $('#password-confirm-signup').val() != $('#password-signup').val()) {
+                e.preventDefault();
+                $('#pc_warning').removeClass('hidden');
+                $('#pc_warning').parent().parent().addClass('has-error');
+              }
             });
-            
-            // Flashy effects for password being under 6 characters.
-            $('#password-signup').on('keyup', function() {
-                if ($(this).val().length < 6) {
-                    $(this).parent().addClass('has-error');
-                    $(this).siblings('p').removeClass('hidden');
-                } else {
-                    $(this).parent().removeClass('has-error');
-                    $(this).siblings('p').addClass('hidden');
-                }
+
+            $('#username-signup').on('keyup keydown', function() {
+              if ($(this).val().length > 0) {
+                $('#username-warning').addClass('hidden');
+                $('#username-warning').parent().parent().removeClass('has-error');
+              }
             });
-            
-            // Effects for mismatching passwords.
-            $(document).on('mouseover', function() {
-                if ($('#password-confirm-signup').val()) {
-                    confirm();
-                }
+
+            $('#password-signup').on('keyup keydown', function() {
+              if ($(this).val().length > 5) {
+                $('#p_warning').addClass('hidden');
+                $('#p_warning').parent().parent().removeClass('has-error');
+              }
             });
-            
-            $('#password-confirm-signup').on('keyup', function() {
-                if ($(this).val()) {
-                    confirm();
-                }
+
+            $('#password-confirm-signup').on('keyup keydown', function() {
+              if ($(this).val() == $('#password-signup').val()) {
+                $('#pc_warning').addClass('hidden');
+                $('#pc_warning').parent().parent().removeClass('has-error');
+              }
             });
-            
-            $('#password-signup').on('keyup', function() {
-                if ($('#password-confirm-signup').val() ) {
-                    confirm();
-                }
-            });
-            
+
         });
--- views/sign-in-register-forms.php
+++ views/sign-in-register-forms.php
@@ -1,24 +1,30 @@
         <div class="container">
             <div class="row">
-                <form method="post" action="sign-in-control.php" class="col-sm-12 col-md-6" id="register-form">
+                <form id="register-form" method="post" action="sign-in-control.php" class="col-sm-12 col-md-6" id="register-form">
                     <h4>Register</h4>
                     <?php if (isset($failed_register_message)) { echo "<div class='alert alert-danger' role='alert'><p>$failed_register_message</p></div>";} ?>
                     <div class="form-group">
-                        <label for="username-signup" class="control-label">Username</label>
+                        <div>
+                          <label for="username-signup" class="control-label">Username</label>
+                          <label for="username-signup" class="hidden pull-right control-label" id="username-warning">Must have a username.</label>
+                        </div>
                         <input type="text" class="form-control" id="username-signup" placeholder="Username" name="username_register">
-                        <p class="hidden control-label">Must have a username.</p>
                     </div>
                     <div class="form-group">
-                        <label for="password-signup" class="control-label">Password</label>
+                        <div>
+                          <label for="password-signup" class="control-label">Password</label>
+                          <label for="password-signup" class="hidden control-label pull-right" id="p_warning">Password must be at least 6 characters.</label>
+                        </div>
                         <input type="password" class="form-control" id="password-signup" placeholder="Password" name="password_register">
-                        <p class="hidden control-label">Password must be at least 6 characters.</p>
                     </div>
                     <div class="form-group">
-                        <label for="password-confirm-signup" class="control-label">Confirm Password</label>
+                        <div>
+                          <label for="password-confirm-signup" class="control-label">Confirm Password</label>
+                          <label for="password-confirm-signup" id="pc_warning" class="hidden pull-right control-label">Passwords do not match.</label>
+                        </div>
                         <input type="password" class="form-control" id="password-confirm-signup" placeholder="Confirm Password">
-                        <p class="hidden control-label">Passwords do not match.</p>
                     </div>
-                    <input type="submit" class="btn btn-primary" value="Register" id="signup-submit" disabled="true">
+                    <input type="submit" class="btn btn-primary" value="Register" id="signup-submit">
                 </form>
                 <form method="post" action="sign-in-control.php" class="col-sm-12 col-md-6">
                     <h4>Login</h4>
