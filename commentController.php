8d70858b2fa447125b80edda81b5f1bab6937e11
JamieKuppel <jamiekuppel@gmail.com>
2016-05-04 20:17:56 -0400

    Comments now only go in one comment chain
    When you comment on a comment it takes you back to the original post
    comment stream but the double comments parent is still the original post
    which might have to be changed.

--- commentController.php
+++ commentController.php
@@ -11,11 +11,13 @@ if (isset($_POST['task']) && $_POST['task'] == 'dropPost') {
   header("Location: view-comments.php?id=" . $_POST['comment_parent']);
   exit();
 } else if (($_POST['task'] == 'addComment' && isset($_POST['title']) && isset($_POST['parent']) && isset($_POST['content']))) {
+  if (isset($_SESSION['parent'])) {$_POST['parent'] = $_SESSION['parent']; unset($_SESSION['parent']);}
   addComment($_POST['title'], $_POST['parent'], $_POST['content'], $_SESSION['username'], $db);
   header("Location: view-comments.php?id=" . $_POST['parent']);
   exit();
 } else if ($_POST['task'] == 'doubleComment') {
-  $singlePost = requestOne($_POST['comment_parent'], $db);
+  $singlePost = requestOne($_POST['comment_id'], $db);
+  $_SESSION['parent'] = $_POST['comment_parent'];
   require('views/user-header.php');
   require('views/create-comment-form.php');
   require('views/create-comment-footer.php');
